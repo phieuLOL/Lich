d781fc17a50ef4a5b21a431b111d7cdc40fd551f
phieuLOL <101406667+phieuLOL@users.noreply.github.com>
2022-05-16 20:07:46 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+var d = new Date();
+var date = d.getDate();  // lấy ngày hôm nay.
+var day = d.getDay();    // lấy thứ của ngày hôm nay.
+var month = d.getMonth() + 1;
+var year = d.getFullYear();
+
+
+document.getElementById('name-month').innerHTML = `Tháng ` + month + ` - ` + year;
+var tam = date;
+// lấy thứ của ngày đầu tiên trong tháng.
+while (tam > 1) {
+     switch (day) {
+          case 0:
+               day = 6;
+               break;
+          default:
+               day -= 1;
+               break;
+     }
+     tam -= 1;
+}
+
+// với cột mốc thứ đâu tiên là chủ nhật, chèn thêm các ô trống để ngày và thứ trong tháng trùng nhau.
+var Gett = document.getElementById('day-of-month');
+for (let index = 0; index < day; index++) {
+     var _class = `day-of-mo false pass`;
+     var html = `<li class="` + _class + `"></li>`
+     Gett.insertAdjacentHTML('beforeend', html);
+}
+
+// lấy số ngày trong tháng
+var maxday = 30;
+switch (month) {
+     case 1, 3, 5, 7, 8, 10, 11:
+          maxday = 31;
+          break;
+
+     case 2:
+          maxday = 28;
+          var nhuan = false;
+          if (year % 100 == 0) {
+               if (year % 4 == 0) {
+                    nhuan = true;
+               }
+          } else {
+               if (year % 4 == 0) {
+                    nhuan = true;
+               }
+          }
+          if (nhuan) maxday = 29;
+          break;
+}
+// in ra các ngày trong tháng.
+for (let index = 1; index <= maxday; index++) {
+     var _class = `day-of-mo false pass`;
+     var _id = ``;
+     if (index == date) {
+          _class = `day-of-mo not`;
+          _id = `true`;
+     } else
+     if (index > date) {
+          _class = `day-of-mo false not`;
+     }
+     var html = `<li class="` + _class + `" id="` + _id + `">` + index + `</li>`
+     Gett.insertAdjacentHTML('beforeend', html);
+}
